174e3f4b1fa0267f2b4bcdb063b50dff187d6f07
Daniel Mendes <senseidanielmendes@gmail.com>
2023-09-02 16:56:32 -0300

    Deploy

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iot38o*ox1@rexz6dw)k-)*$0tvdev3p_vgrzpbkgi^whwfn&2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
