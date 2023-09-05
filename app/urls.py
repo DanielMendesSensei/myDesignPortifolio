102b1b6037f6f48914f4c85983244fcfea331d8f
Daniel Mendes <senseidanielmendes@gmail.com>
2023-09-05 16:40:20 -0300

    Wireframe Update

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'app'
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('vector/', views.vector, name='vector'),
+    path('logos/', views.logos, name="logos"),
+    path('pixel/', views.pixel, name="pixel"),
+    path('ui_ux/', views.ui_ux, name="ui_ux"),
+    path('general/', views.general, name="general")
 ]
--- app/views.py
+++ app/views.py
@@ -1,10 +1,44 @@
 from django.shortcuts import render
 
 def home(request):
-    context = {'title':'Sensei Daniel Mendes',
-               'copyrigth': 'Copyrigth Daniel Mendes',
-               'hard_skills':'Hard Skills',
-               'soft_skills':'Soft Skills',
-               'portfolio':'All The Little & Big Things'}
+    context = {'title':'Daniel Mendes',
+               'copy':'Copyright© 2023 Daniel Mendes Design Portfolio',
+               }
     return render(request, 'index.html', context)
+
+def vector(request):
+    context = {'title':'Vectors - Daniel Mendes',
+               'page_title':'DESIGN GRÁFICO - VETORES',
+               'copy':'Copyright© 2023 Daniel Mendes Design Portfolio',
+               }
+    return render(request, 'vector.html', context)
+
+def logos(request):
+    context = {'title':'Logos - Daniel Mendes',
+               'page_title':'DESIGN GRÁFICO - LOGOS',
+               'copy':'Copyright© 2023 Daniel Mendes Design Portfolio',
+               }
+    return render(request, 'logos.html', context)
+
+def pixel(request):
+    context = {'title':'Pixel Art - Daniel Mendes',
+               'page_title':'DESIGN DE GAMES - PIXEL ART',
+               'copy':'Copyright© 2023 Daniel Mendes Design Portfolio',
+               }
+    return render(request, 'pixel.html', context)
+
+def ui_ux(request):
+    context = {'title':'UI/UX - Daniel Mendes',
+               'page_title':'DESIGN - UI/UX',
+               'copy':'Copyright© 2023 Daniel Mendes Design Portfolio',
+               }
+    return render(request, 'ui_ux.html', context)
+
+def general(request):
+    context = {'title':'Geral - Daniel Mendes',
+               'page_title':'DESIGN EM GERAL',
+               'copy':'Copyright© 2023 Daniel Mendes Design Portfolio',
+               }
+    return render(request, 'general.html', context)
+
 # Create your views here.
